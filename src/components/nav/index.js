93a2546917b6ea858a288c66b2c47fd4f658adac
Tiago Vasconcelos <talvasconcelos@gmail.com>
2017-09-05 23:52:49 +0100

    First commit

---
+++ src/components/nav/index.js
@@ -0,0 +1,27 @@
+import { h, Component } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style';
+
+export default class Nav extends Component {
+	render() {
+		return (
+			<header class={style.header}>
+        <nav  class={style.navBar}>
+          <input type="checkbox" id='menu-toggle' class={style.menuToggle}/>
+          <label for="menu-toggle" class={style.labelToggle}></label>
+          <ul>
+            <li>
+              <Link activeClassName={style.active} href="/">Home</Link>
+            </li>
+            <li>
+              <Link activeClassName={style.active} href="/profile">Me</Link>
+            </li>
+            <li>
+              <Link activeClassName={style.active} href="/profile/john">John</Link>
+            </li>
+          </ul>
+        </nav>
+			</header>
+		);
+	}
+}
